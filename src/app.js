989e63f75eb73e0db4b70f31cbd1e9a29a52aa18
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-10-12 09:58:35 +0530

    seperating index.js and cors

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ app.use(express.static('public'));
 // cors configuration
 app.use(cors({
     origin: process.env.CORS_ORIGIN?.split(',') || 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 
