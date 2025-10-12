7d8da514ac1ff6294786974beb0f727c350db90f
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-10-12 09:56:33 +0530

    Cors configuration and app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
+
+// Middleware for parsing JSON and urlencoded data
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({ extended: true , limit: '16kb' }));
+// Public folder data access
+app.use(express.static('public'));
+
+// cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(',') || 'http://localhost:5173',
+}));
 
 
 app.get('/', (req, res) => {
